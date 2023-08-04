a97a470f1aa122d343fb0765d4001158fd620390
huangbenliang <huangbenliang@pyou.com>
2023-08-04 18:56:06 +0800

    修改编译方式

---
+++ server/LD_ServerKernel/src/server_kernel.cc
@@ -0,0 +1,9 @@
+//
+// Copyright [2023-2043] LaglessDesk Holding
+//
+
+#include "server_kernel.h"
+
+int main() {
+  return 0;
+}
---
+++ server/LD_ServerKernel/src/server_kernel.h
@@ -0,0 +1,20 @@
+//
+// Copyright [2023-2043] LaglessDesk Holding
+//
+#pragma once
+
+#include <string>
+
+namespace lage::server::kernel {
+class ServerKernel {
+ public:
+  ServerKernel();
+  ~ServerKernel();
+
+ private:
+};
+
+ServerKernel::ServerKernel() {}
+
+ServerKernel::~ServerKernel() {}
+}  // namespace lage::server::kernel
